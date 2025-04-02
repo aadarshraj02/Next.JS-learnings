90cabfdd723a80cf27ce1300e87fdc48d34e4fe4
adarsh02 <aadarshraazz007@gmail.com>
2025-04-02 17:00:48 +0530

    done with useRoute hooks

--- linking/src/app/page.tsx
+++ linking/src/app/page.tsx
@@ -1,4 +1,24 @@
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 const Home = () => {
-  return <div>Home</div>;
+  const router = useRouter();
+  console.log(router);
+
+  const navigate = (page: string) => {
+    router.push(`${page}`);
+  };
+
+  return (
+    <div>
+      <button onClick={() => navigate("/login")} className="cursor-pointer">
+        Go To the Login Page
+      </button>
+      <br />
+      <Link href={"/colors"}>Go to colors page</Link>
+    </div>
+  );
 };
 export default Home;
