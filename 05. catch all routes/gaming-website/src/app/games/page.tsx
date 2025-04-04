4a62d7c61fe6ec4f3b1380da67046a33a4bfed14
adarsh02 <aadarshraazz007@gmail.com>
2025-04-04 21:14:49 +0530

    styled games page

--- 05. catch all routes/gaming-website/src/app/games/page.tsx
+++ 05. catch all routes/gaming-website/src/app/games/page.tsx
@@ -1,4 +1,36 @@
+/* eslint-disable @next/next/no-img-element */
+import { games } from "@/app/data/games";
+import Link from "next/link";
+console.log(games);
+
 const GamePage = () => {
-  return <div>GamePage</div>;
+  return (
+    <div className="container mx-auto p-6">
+      <h1 className="text-3xl font-bold text-center mb-8">Explore Games</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        {games.map((game) => (
+          <Link
+            className="group relative bg-gray-900 p-5 rounded-xl shadow-lg transform transition hover:scale-105 hover:shadow-xl"
+            key={game.id}
+            href={`/games/${game.category}/${game.slug}`}
+          >
+            <div className="relative">
+              <img
+                className="w-full h-52 object-cover rounded-lg group-hover:opacity-80 transition"
+                src={game.image}
+                alt={game.title}
+              />
+              <div className="absolute inset-0 bg-black bg-opacity-30 rounded-lg opacity-0 group-hover:opacity-100 transition"></div>
+            </div>
+            <h2 className="text-2xl font-semibold text-white mt-4">
+              {game.title}
+            </h2>
+            <p className="text-gray-400 mt-2 text-sm">{game.description}</p>
+            <p className="text-yellow-400 font-medium mt-3">⭐ {game.rating}</p>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default GamePage;
--- 05. catch all routes/gaming-website/src/app/page.tsx
+++ 05. catch all routes/gaming-website/src/app/page.tsx
@@ -1,4 +1,12 @@
+import Link from "next/link";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="h-screen w-full flex items-center justify-center">
+      <Link href="/games" className="text-3xl text-red-700/70">
+        Go To games Page
+      </Link>
+    </div>
+  );
 };
 export default Home;
