44d7e6fd5871ed31956fe115993c5c2892ae8ff9
adarsh02 <aadarshraazz007@gmail.com>
2025-04-07 19:09:48 +0530

    designed aside bar

--- 06. Layout/layout-challange/src/app/layout.tsx
+++ 06. Layout/layout-challange/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { ReactNode } from "react";
+import "./globals.css";
 
 const BaseLayout = ({ children }: { children: ReactNode }) => {
   return (
@@ -10,17 +11,53 @@ const BaseLayout = ({ children }: { children: ReactNode }) => {
             <nav>
               <ul className="flex space-x-4">
                 <li>
-                  <Link href={"/"}>Home</Link>
+                  <Link className="hover:text-purple-400" href={"/"}>
+                    Home
+                  </Link>
                 </li>
                 <li>
-                  <Link href={"/about"}>About</Link>
+                  <Link className="hover:text-purple-400" href={"/about"}>
+                    About
+                  </Link>
                 </li>
                 <li>
-                  <Link href={"/contact"}>Contact</Link>
+                  <Link className="hover:text-purple-400" href={"/contact"}>
+                    Contact
+                  </Link>
                 </li>
               </ul>
             </nav>
           </header>
+          <div className="flex flex-1">
+            <aside className="w-64 bg-purple-200 p-4">
+              <ul>
+                <li>
+                  <Link
+                    href={"/category/technology"}
+                    className="block py-2 hover:bg-purple-300"
+                  >
+                    Technology
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href={"/category/design"}
+                    className="block py-2 hover:bg-purple-300"
+                  >
+                    Design
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href={"/category/business"}
+                    className="block py-2 hover:bg-purple-300"
+                  >
+                    Business
+                  </Link>
+                </li>
+              </ul>
+            </aside>
+          </div>
         </div>
         {children}
       </body>
