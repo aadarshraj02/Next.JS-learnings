265d0fdd83f055b293bc83bfeb94aa44e5750d12
adarsh02 <aadarshraazz007@gmail.com>
2025-04-08 09:03:20 +0530

    what happens in layout page

--- 07. Templates/templates/src/app/analytics/layout.tsx
+++ 07. Templates/templates/src/app/analytics/layout.tsx
@@ -1,4 +1,24 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+
 const Layout = () => {
-  return <div>Layout</div>;
+  const [name, setName] = useState("");
+
+  return (
+    <div className="m-10">
+      <input
+        type="text"
+        value={name}
+        placeholder="Enter Name"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <br />
+      <Link href={"/analytics/revenue"}>Revenue</Link>
+      <br />
+      <Link href={"/analytics/stats"}>Stats</Link>
+    </div>
+  );
 };
 export default Layout;
