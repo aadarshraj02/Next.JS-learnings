c95fae69b885f745116c8c15a222ea9672520fd0
adarsh02 <aadarshraazz007@gmail.com>
2025-04-09 11:14:30 +0530

    added function to updated movies by id

--- 10. backend/Dynamic routes/movies/[id]/route.ts
+++ 10. backend/Dynamic routes/movies/[id]/route.ts
@@ -11,3 +11,48 @@ export async function GET(
     ? new Response(JSON.stringify(movie))
     : new Response(null, { status: 404 });
 }
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  const movieId = +id;
+
+  const movie = movies.find((m) => m.id === movieId);
+
+  if (!movie) {
+    return new Response(
+      JSON.stringify({
+        error: "Movie not found",
+      })
+    );
+  }
+
+  try {
+    const updatedMovie = await req.json();
+
+    const index = movies.findIndex((m) => m.id === movieId);
+
+    if (index === -1) {
+      return new Response(
+        JSON.stringify({
+          error: "Movie not found",
+        }),
+        {
+          status: 404,
+        }
+      );
+    }
+    movies[index] = { ...movie, ...updatedMovie };
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        error: "Failed to patch movie",
+      }),
+      {
+        status: 400,
+      }
+    );
+  }
+}
