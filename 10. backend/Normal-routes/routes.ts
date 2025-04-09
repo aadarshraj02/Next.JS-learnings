d2ed0ddeeef8abbb66e5ee148938cf503b1565b5
adarsh02 <aadarshraazz007@gmail.com>
2025-04-09 11:26:40 +0530

    added route for redirect

--- 10. backend/Normal-routes/routes.ts
+++ 10. backend/Normal-routes/routes.ts
@@ -1,4 +1,5 @@
 import { movies } from "./db";
+import { NextRequest } from "next/server"; //nextjs not installed so its giving error
 
 export async function GET() {
   return Response.json(movies);
@@ -11,4 +12,14 @@ export async function POST(req: Request) {
 
   movies.push(newMovie);
   return new Response(JSON.stringify(newMovie));
+}
+
+export async function GET(request: NextRequest) {
+  //giving error as two get function cant be in same file
+  const searchParams = request.nextUrl.sendParams;
+  const query = searchParams.get("query");
+
+  const filteredMovies = query
+    ? movies.filter((m) => m.name.includes(query))
+    : movies;
 }
