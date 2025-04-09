efaa7ec8f3c7ebb5cb02166a6ded08c24bba3e64
adarsh02 <aadarshraazz007@gmail.com>
2025-04-09 11:01:44 +0530

    added route to post new movie

--- 10. backend/Get-method/route.ts
+++ 10. backend/Get-method/route.ts
@@ -1,5 +1,16 @@
 import { movies } from "./db";
 
+//Get all movies routes
+
 export async function GET() {
+  return Response.json(movies);
+}
+
+export async function POST(request: Request) {
+  let movie = await request.json();
+  console.log("Movies", movie);
+
+  const newMovie = { ...movie };
+  movies.push(newMovie);
   return Response.json(movies);
 }
